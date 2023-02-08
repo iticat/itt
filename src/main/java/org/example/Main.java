0f954a56807fc920428df4916703aebe20ace840
itcat <itcat@itcast.cn>
2023-02-08 17:27:19 +0800

    commit1

---
+++ src/main/java/org/example/Main.java
@@ -0,0 +1,10 @@
+package org.example;
+
+public class Main {
+    public static void main(String[] args) {
+
+        System.out.println("Hello world!");
+        System.out.println("xzq");
+        System.out.println("liuqsong");
+    }
+}
