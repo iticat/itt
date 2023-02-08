da8d5a484e7a6d8b0ba331b0c8fc6bd9215733c0
itcat <itcat@itcast.cn>
2023-02-08 17:40:23 +0800

    commit3

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -6,7 +6,6 @@ public class Main {
         System.out.println("Hello world!");
         System.out.println("xzq");
         System.out.println("liuqsong");
-        System.out.println("liuqsong");
         System.out.println("liuqsong2");
     }
 }
